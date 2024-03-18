5f6c3090c5bb459e84e7f9ba869d41c2e1a77b7e
Durgesh7758 <132260287+Durgesh7758@users.noreply.github.com>
2024-03-18 07:46:52 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,73 @@
+// let  score = "Darsh"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+// let score = "Daresh"
+// console.log(typeof score);
+// let ValueInNumber = Number(score)
+// console.log(ValueInNumber);
+
+
+// true => 1
+// false => 0
+// " " => false
+// "Darsh" => true
+
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(someNumber);
+// console.log(typeof someNumber);
+
+// console.log((+""));
+// console.log((+true));
+
+// let num1,num2,num3
+// num1= num2= num3 = 2+2
+
+
+// *************************** OPERATIONS ************************* //
+
+
+// let value = 3
+// let value1 = -value
+// console.log(value1);
+
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+// ++gameCounter;
+// console.log(gameCounter);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2%2)
+// console.log(2**2)
+// console.log(2>=2)
+
+// let str1 = "Ram"
+// let str2 = "Darsh"
+// let str3 = str1 + str2
+
+// console.log(str3);
+// console.log(str1 + str2 );
+// console.log(str2 + str1);
+
+// console.log("1" + 2);
+// console.log("2" + 1);
+// console.log("1" + 1 + 2);
+// console.log(1 + 2 + "3");
+
+
+// console.log(+true);
+// console.log(+"");
+
+
