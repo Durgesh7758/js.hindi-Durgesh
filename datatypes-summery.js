c1296e99fc3469e8045662517b3339eabc81eb2e
Durgesh7758 <132260287+Durgesh7758@users.noreply.github.com>
2024-03-19 06:59:32 +0000

    datatypes-summery

---
+++ datatypes-summery.js
@@ -0,0 +1,35 @@
+// Primitive Datatypes
+// 7 Types : String, Number, Boolean, null, undefined, symbol, BigInt 
+
+
+// References (Non Primitive) Datatypes
+// Array, Objects, Functions
+
+
+// const score = 100
+// console.log(score);
+
+// const id = Symbol("123")
+// const anotherId  = Symbol("123")
+
+// console.log(id == anotherId);
+// console.log(typeof id);
+// console.log(typeof anotherId);
+// console.log([id,anotherId]);
+// console.log(id);
+
+const heros = ["Brother","Father","Uncle"]
+console.log(heros);
+
+let myObject = {
+    "name":"Darsh",
+    "age":20,
+    "place":"Thane"
+}
+console.log(myObject);
+
+const myFunction = function(){
+    console.log("Hello World...!");
+    console.log(myFunction)
+}
+
