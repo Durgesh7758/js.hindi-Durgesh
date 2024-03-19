3ec571ba1e16536d0feb2435e02473d0c5e277e6
Durgesh7758 <132260287+Durgesh7758@users.noreply.github.com>
2024-03-19 13:12:09 +0000

    05_strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,21 @@
+const name = "hitesh"
+const repoCount = 50
+
+console.log(name + repoCount + " value");
+
+console.log(`Hello My name is ${name} and my repo count is ${repoCount}`);
+ 
+
+const gameName = new String("Darsh")
+
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase())
+// console.log(gameName.split())
+
+const mewString = gameName.substring(0,4)
+console.log(mewString);
+
+const gameName1 = (gameName.indexOf('h'))
+console.log(gameName1);
+
