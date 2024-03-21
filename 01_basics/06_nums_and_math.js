65c19c73a358aa2165766826dbc7c4f365c9bdfd
Durgesh7758 <132260287+Durgesh7758@users.noreply.github.com>
2024-03-21 06:56:21 +0000

    06_nums_and_math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,34 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+// console.log(balance.toString(2));
+
+// const otherNumber = 123.952300
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 10000
+// console.log(hundreds.toLocaleString('en-In'));
+
+
+
+
+
+//  *******************************  Math  ***********************************  \\
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+// console.log(Math);
+// console.log(Math.abs(-4.9));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(4.9));
+// console.log(Math.floor(5.1));
+// console.log(Math.max(45,35,25,67,432));
+// console.log(Math.min(45,35,25,67,432));
+
+console.log(Math.random());
