3c86842e68c067b455b8b4bb250ba9dc0e64d0fe
Durgesh7758 <132260287+Durgesh7758@users.noreply.github.com>
2024-03-24 07:54:01 +0000

    07_datesinJs

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,25 @@
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(2024 , 3, 23)
+// let myCreateDate1 = new Date("2024-06-11")
+// console.log(myCreateDate.toDateString());
+// console.log(myCreateDate1.toDateString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date()
+// console.log(newDate.getDay());
+// console.log(newDate.getDate());
+// console.log(newDate.getHours());
+// console.log(newDate.getMonth() + 5);
+
+// newDate.toLocaleString('default',{
+//     weekday: "long"
+    
+// })
